861e09d2a3a1651a76e3bb021fc83e9bf1423948
Alex-rgb62 <johnalexisboliza@gmail.com>
2026-08-11 23:18:20 +0800

    Add files via upload

---
+++ tanza-public-service/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Municipality of Tanza Public Service Portal</title>
+</head>
+
+<body>
+
+    <h1>Municipality of Tanza</h1>
+
+    <p>Public Service Information and Request Management System</p>
+
+</body>
+</html>
